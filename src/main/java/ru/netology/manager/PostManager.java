0ea6f40fb08c50f92847212b1bef771cb7ac83ca
irikras <irikras@mail.ru>
2021-08-11 13:46:45 +0300

    PostManager.java add

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,14 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+    private Post[] posts;
+
+    public Post[] search(int owner_id, String domain, String query, boolean owners_only, int count, int offset) {
+        return null;
+    }
+
+    public void delete(int owner_id, int post_id) {
+    }
+}
